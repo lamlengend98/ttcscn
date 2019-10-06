055b516bf0ed032451a4f628caff73274ddae040
lamt <lamnguyen98tb@gmail.com>
2019-10-06 21:32:55 +0700

    api admin

---
+++ mobella/src/main/java/com/java/mobella/MobellaApplication.java
@@ -0,0 +1,13 @@
+package com.java.mobella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobellaApplication.class, args);
+	}
+
+}
